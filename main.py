531a611eaf2d771ba312f02451a5d53042843252
anirudhwillcode <anirudhvps57@gmail.com>
2025-10-16 12:54:45 +0530

    Star commit Added few changes

--- main.py
+++ main.py
@@ -4,44 +4,56 @@ from google import genai
 import sys
 from google.genai import types
 
-load_dotenv()
-api_key=os.environ.get("GEMINI_API_KEY")
-client =  genai.Client(api_key=api_key)
+
+
 
 def main():
+    load_dotenv()
 
     # When you run your python script like this python myscript.py " Who is Chris"
     # Python puts everything you type into a list called sys.argv 
     # the list looks like this : ["myscript.py", "Who", "world"]
-   
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("=="):
+            args.append(arg)
 
-    args = sys.argv[1:]
+
 
     if not args:
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt here"')
         sys.exit(1)
+    
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
+    
     user_prompt=" ".join(args)
+
+    if verbose:
+        print(f"User Prompt: {user_prompt}\n")
 
     messages = [
         types.Content(role="user",parts =[types.Part(text=user_prompt)])
     ]
 
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001',
-        contents= messages,
-     )
-    
-    if "--verbose" in user_prompt:
+    generate_content(client,messages,verbose)
 
-        
-        print("---------------------------------------------")
-        print(response.text)
-        print(f"User prompt: {user_prompt}")
+def generate_content(client,messages,verbose):
+
+    response = client.models.generate_content(
+    model='gemini-2.0-flash-001',
+    contents= messages,
+)
+    
+    if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    else:
-        print(response.text)
+    print("Response:")
+    print(response.text)
     
 
 if __name__ == "__main__":
